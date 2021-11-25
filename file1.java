c449b03cb6fdc63dcd4550fcc5a9954a5c960f9d
harshisingh09 <94909536+harshisingh09@users.noreply.github.com>
2021-11-25 17:39:52 +0530

    Update file1.java

--- file1.java
+++ file1.java
@@ -2,7 +2,7 @@ class Simple{
     public static void main(String args[]){  
      int a = 6;
      String b = "Hello Dolly";
-     System.out.println(a+" "+b);  
+     System.out.println(b+" "+a);  
      System.out.println(a);
     }  
 }
